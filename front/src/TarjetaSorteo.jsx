1d6613190393e2737fef5a03bd31c1cb433534ca
JonathanAstorga <jonathan.astorga245791@potros.itson.edu.mx>
2025-11-05 23:06:29 -0700

    front inicio

---
+++ front/src/TarjetaSorteo.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+//import './GaleriaSorteos.css'; 
+
+export default function TarjetaSorteo({ sorteo }) {
+  
+  const formatoPrecio = new Intl.NumberFormat('es-MX', {
+    style: 'currency',
+    currency: 'MXN',
+    minimumFractionDigits: 2,
+  });
+
+  return (
+    // Usamos el data-testid del TC-HU05-001 [cite: 33]
+    <div className="raffle-card" data-testid="raffle-card">
+      <img 
+        src={sorteo.image_url} 
+        alt={sorteo.name} 
+        className="raffle-image" 
+      />
+      <div className="raffle-info">
+        <h3 className="raffle-name">{sorteo.name}</h3>
+        <p className="raffle-price">
+          {formatoPrecio.format(sorteo.price_per_number)}
+        </p>
+      </div>
+    </div>
+  );
+}
